e1d58c0ae67a971b43f8fd39ca1678592744d1e6
urosrelicc <urosrelicc@gmail.com>
2023-12-15 13:02:53 +0100

    add exception handling

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const img = document.querySelector('img');
 
 async function getCats() {
-    const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=2Mrrc6OUM1eMGO20UFUr16Wl802EK11L&s=cats', {mode: 'cors'})
-    const catData = await response.json();
-    img.src = catData.data.images.original.url;
+    try {
+        const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=2Mrrc6OUM1eMGO20UFUr16Wl802EK11L&s=cats', {mode: 'cors'})
+        const catData = await response.json();
+        img.src = catData.data.images.original.url;
+    } catch(error) {
+        img.alt = error;
+    }
+
 
 }
 
